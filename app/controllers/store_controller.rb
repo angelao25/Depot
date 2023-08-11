e013dc403c1a68fd446faf8d34a2b777ea3144b7
Angel Aguirre Obando <anngelao25@gmail.com>
2023-08-11 18:59:45 -0500

    Cart and Sessions

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
+    #Check if the session counter variable is nil
+    if session[:counter].nil?
+      session[:counter] = 1 # Initialize the counter
+    else
+      session[:counter] += 1
+    end
     @products = Product.order(:title)
   end
 end
